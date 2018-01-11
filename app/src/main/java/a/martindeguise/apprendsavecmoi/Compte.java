d716e3f01ab433b74264a8b1fca93b136b4498c0
martindeg <martindeguise@gmail.com>
2018-01-11 22:21:35 +0100

    ajout du layout pour la classe fais des calculs version 2

--- app/src/main/java/a/martindeguise/apprendsavecmoi/Compte.java
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/Compte.java
@@ -1,7 +1,10 @@
 package a.martindeguise.apprendsavecmoi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Compte extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Compte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_compte);
+
+        final Button faisDesCalculsButton = (Button) findViewById(R.id.button2);
+        faisDesCalculsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Compte.this, FaisDesCalculs.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/a/martindeguise/apprendsavecmoi/FaisDesCalculs.java
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/FaisDesCalculs.java
@@ -2,26 +2,47 @@ package a.martindeguise.apprendsavecmoi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import java.lang.Math;
 
 public class FaisDesCalculs extends AppCompatActivity {
 
+    private int nb1, nb2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_compte);
-        int resultat = genererAddition();
+        setContentView(R.layout.layout_fais_des_calculs);
+
+        // Affichage de l'addition
+        TextView textView4 = (TextView)findViewById(R.id.textView4);
+        textView4.setText(genererAddition());
+
+        // Affichage du resultat
+        // TextView textView5 = (TextView)findViewById(R.id.textView5);
+        // textView5.setText(String.valueOf(resultat()));
     }
 
     private int generateurNombreAleatoire(int n){
         return (int) (Math.random() * n); //Pour un entier entre 0 et n-1
     }
 
-    public int genererAddition(){
-        int nb1 =  generateurNombreAleatoire(10);
-        int nb2 =  generateurNombreAleatoire(10);
-        int result = nb1 + nb2;
-        System.out.println(nb1 + "+" + nb2 +"= ?");
-        return result;
+    public String genererAddition(){
+        nb1 =  generateurNombreAleatoire(10);
+        nb2 =  generateurNombreAleatoire(10);
+        return nb1 + "+" + nb2 +"= ?";
+    }
+
+    public int getNb1(){
+        return nb1;
+    }
+
+    public int getNb2(){
+        return nb2;
+    }
+
+    public int resultat(){
+        return nb1 + nb2;
     }
 }
