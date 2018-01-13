792c285c4f3a4a4754d290902cd44a8e0070328d
martindeg <martindeguise@gmail.com>
2018-01-13 12:30:41 +0100

    Ajout de la classe compteObjet /!\ Dans resultat -> reprendre le if pour dire a l'enfant si ce qu'il a fait c'est bon ou pas bon Fin de la partie compte avec moi. Il faut juste regarder la partie graphique de l'application et reprendre l'affichage des resultat

---
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/acceuil.java
@@ -0,0 +1,42 @@
+package a.martindeguise.apprendsavecmoi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * Created by martin on 13/01/2018.
+ */
+
+public class Acceuil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_acceuil);
+
+        // Création bouton compte avec moi !
+        final ImageButton compte = findViewById(R.id.imageButton);
+        compte.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Acceuil.this, Compte.class);
+                startActivity(intent);
+            }
+        });
+
+        // Création bouton ecris avec moi
+        final ImageButton compteObjetsButton= findViewById(R.id.imageButton2);
+        compteObjetsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Acceuil.this, Compte.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
