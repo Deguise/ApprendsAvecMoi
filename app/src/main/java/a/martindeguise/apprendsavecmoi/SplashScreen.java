edd66d3a563e7f5441e537fd699fef4bb529b5e3
Leo <leo-vivant@hotmail.fr>
2018-01-16 18:10:32 +0100

    Ajout du logo, de la page de chargement qui marche + de fichier xml pour gérer les dimensions tout support

--- app/src/main/java/a/martindeguise/apprendsavecmoi/SplashScreen.java
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/SplashScreen.java
@@ -1,10 +1,12 @@
 package a.martindeguise.apprendsavecmoi;
 
 //import activity_splash;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -12,17 +14,16 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     private static int SPLASH_TIME_OUT = 3000;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
-
-        new Handler().postDelayed(new Runnable() {
+         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run(){
