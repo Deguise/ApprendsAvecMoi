0f668a414a51b69fb997dd57658298f6a45c3f6d
martindeg <martindeguise@gmail.com>
2018-03-10 09:41:52 +0100

    Ajout de la classe DicteeDeMots avec la classe Word et Resulats_Dictee Avec tous les Layout (layout pas encore mis en forme

---
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/DicteeDeMots.java
@@ -0,0 +1,43 @@
+package a.martindeguise.apprendsavecmoi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DicteeDeMots extends AppCompatActivity {
+
+    final String EXTRA_RESULTATUSER = "resultatUser";
+    final String EXTRA_RESULTAT = "resultat";
+
+    private Word mot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mot = new Word();
+        setContentView(R.layout.layout_dictee_mots);
+
+        // Affichage du mot que l'enfant doit ecrire
+        TextView textView5 = findViewById(R.id.textView5);
+        textView5.setText(mot.getSecretWord());
+
+        final EditText resultat = findViewById(R.id.edittext); // AJOUTER LA EDIT TEXT
+
+        final Button okButton = findViewById(R.id.button5);
+        okButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DicteeDeMots.this, Resultat_DicteeMots.class);
+                intent.putExtra(EXTRA_RESULTATUSER, resultat.getText().toString());
+                intent.putExtra(EXTRA_RESULTAT, mot.getSecretWord());
+                startActivity(intent);
+            }
+        });
+    }
+
+}
