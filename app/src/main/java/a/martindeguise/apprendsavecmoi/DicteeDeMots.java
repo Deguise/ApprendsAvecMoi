60335528edf43124ef3c63c013576229fbfa5dde
martindeg <martindeguise@gmail.com>
2018-03-10 23:30:47 +0100

    Amelioration Layout Resultat

--- app/src/main/java/a/martindeguise/apprendsavecmoi/DicteeDeMots.java
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/DicteeDeMots.java
@@ -8,19 +8,55 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DicteeDeMots extends AppCompatActivity {
 
     final String EXTRA_RESULTATUSER = "resultatUser";
     final String EXTRA_RESULTAT = "resultat";
-
-    private Word mot;
+    private String word = "mot de base";
+    private static String filepath = "dictionnaire.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mot = new Word();
         setContentView(R.layout.layout_dictee_mots);
+
+        // Determiner la ligne du mot dans le dictionnaire
+        int i = (int)(Math.random() * 100000);
+        while(i > 336529){
+            i /= 2;
+        }
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                new InputStreamReader(getAssets().open(filepath)));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            int compteur = 0;
+            while ((mLine = reader.readLine()) != null) {
+                compteur++;
+                if(compteur == i){
+                    word = mLine;
+                    break;
+               }
+            }
+        } catch (IOException e) {
+            word = e.toString();
+        } finally {
+            if (reader != null) {
+                 try {
+                     reader.close();
+                 } catch (IOException e) {
+                     word = e.toString();
+                 }
+            }
+        }
 
         // Création bouton retour accueil avec moi
         final ImageButton accueilButton= findViewById(R.id.imageButton3);
@@ -35,10 +71,8 @@ public class DicteeDeMots extends AppCompatActivity {
 
         // Affichage du mot que l'enfant doit ecrire
         TextView textView25 = findViewById(R.id.textView25);
-        textView25.setText(mot.getWord());
+        textView25.setText(word);
 
-        // Affichage dans le terminal du mot à dicter
-        //System.out.println(mot.getWord());
 
         final EditText resultat = findViewById(R.id.edittext22); // AJOUTER LA EDIT TEXT
 
@@ -49,7 +83,7 @@ public class DicteeDeMots extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(DicteeDeMots.this, Resultat_DicteeMots.class);
                 intent.putExtra(EXTRA_RESULTATUSER, resultat.getText());
-                intent.putExtra(EXTRA_RESULTAT, mot.getWord());
+                intent.putExtra(EXTRA_RESULTAT, word);
                 startActivity(intent);
             }
         });
