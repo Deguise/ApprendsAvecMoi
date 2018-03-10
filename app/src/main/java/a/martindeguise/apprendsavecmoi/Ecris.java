d1f897fd4c27712862df04a57dba195cdff9b59d
martindeg <martindeguise@gmail.com>
2018-03-10 10:17:34 +0100

    Ajout des layout pour dictee de mots

--- app/src/main/java/a/martindeguise/apprendsavecmoi/Ecris.java
+++ app/src/main/java/a/martindeguise/apprendsavecmoi/Ecris.java
@@ -6,35 +6,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
-/**
- * Created by martin on 13/01/2018.
- */
-
-public class Acceuil extends AppCompatActivity {
+public class Ecris extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_acceuil);
+        setContentView(R.layout.layout_ecrit);
 
-        // Création bouton compte avec moi !
-        final ImageButton compte = findViewById(R.id.imageButton);
-        compte.setOnClickListener(new View.OnClickListener() {
+        // Création bouton Trace la lettre
+        final ImageButton faisDesCalculsButton = findViewById(R.id.imageButton);
+        faisDesCalculsButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Acceuil.this, Compte.class);
+                Intent intent = new Intent(Ecris.this, Acceuil.class);
                 startActivity(intent);
             }
         });
 
-        // Création bouton ecris avec moi
+        // Création bouton dictee de mots
         final ImageButton compteObjetsButton= findViewById(R.id.imageButton2);
         compteObjetsButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Acceuil.this, Ecris.class);
+                Intent intent = new Intent(Ecris.this, DicteeDeMots.class);
                 startActivity(intent);
             }
         });
